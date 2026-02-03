5a7bcca1cc088832926b92ce12ae0bf88ea78dcc
VidhyamahalakshmiS <vidhya230905@gmail.com>
2026-02-03 11:01:37 +0530

    Add files via upload

---
+++ DSA/Sorting/SortDes.java
@@ -0,0 +1,28 @@
+package DSA.Sorting.Basic;
+import java.util.Scanner;
+public class SortDes {
+   public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int []arr=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    sort(arr);
+    sc.close();
+   } 
+   static void sort(int[]arr){
+    for(int i=0;i<arr.length;i++){
+        for(int j=1;j<arr.length-i;j++){
+            if(arr[j]>arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+            }
+        }
+    }
+    for(int k=0;k<arr.length;k++){
+        System.out.print(arr[k]+" ");
+    }
+   }
+}
