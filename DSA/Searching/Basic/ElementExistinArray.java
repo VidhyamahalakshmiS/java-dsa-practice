59259ca78e3451f7cdf17ad9b6b51f5bb1fcfe14
VidhyamahalakshmiS <vidhya230905@gmail.com>
2026-02-04 09:31:02 +0530

    Add files via upload

---
+++ DSA/Searching/Basic/ElementExistinArray.java
@@ -0,0 +1,19 @@
+package DSA.Searching.Basic;
+import java.util.Scanner;
+public class ElementExistinArray {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]==target){
+                System.out.println("Target found: "+arr[i]);
+            }
+        }
+        sc.close();
+    }
+}
